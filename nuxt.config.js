8e18bbd3e88bdb9a94d4a0a87b458ea4dd9235f3
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2020-12-12 11:57:21 -0500

    fix site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || 'Rob Wilkinson\'s Blog',
+    title: 'Rob Wilkinson\'s Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
